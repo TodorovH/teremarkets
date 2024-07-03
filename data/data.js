229dba8ab9ff77c07b59710b48933967f75af21b
TodorovH <hristo_petkov2001@abv.bg>
2024-07-03 17:19:08 +0300

    Updates

---
+++ data/data.js
@@ -0,0 +1,65 @@
+export const ARTICLES = [
+  {
+    id: 1,
+    imgUrl: `/images/articles/article1.png`,
+    title: `USA, GA - Milton Eagles Football`,
+    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+      `,
+  },
+  {
+    id: 2,
+    imgUrl: `/images/articles/article2.png`,
+    title: `USA, GA - Milton Eagles Football`,
+    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+      `,
+  },
+  {
+    id: 3,
+    imgUrl: `/images/articles/article3.png`,
+    title: `USA, GA - Milton Eagles Football`,
+    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+      `,
+  },
+  {
+    id: 4,
+    imgUrl: `/images/articles/article4.png`,
+    title: `USA, GA - Milton Eagles Football`,
+    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+      `,
+  },
+  {
+    id: 5,
+    imgUrl: `/images/articles/article5.png`,
+    title: `USA, GA - Milton Eagles Football`,
+    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+      `,
+  },
+  {
+    id: 6,
+    imgUrl: `/images/articles/article6.png`,
+    title: `USA, GA - Milton Eagles Football`,
+    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+      `,
+  },
+  {
+    id: 7,
+    imgUrl: `/images/articles/article7.png`,
+    title: `USA, GA - Milton Eagles Football`,
+    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+      `,
+  },
+  {
+    id: 8,
+    imgUrl: `/images/articles/article8.png`,
+    title: `USA, GA - Milton Eagles Football`,
+    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+      `,
+  },
+  {
+    id: 9,
+    imgUrl: `/images/articles/article9.png`,
+    title: `USA, GA - Milton Eagles Football`,
+    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+      `,
+  },
+];
