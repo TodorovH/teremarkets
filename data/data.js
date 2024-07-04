d9edfcda17e420478e5b6575b763246503825894
TodorovH <hristo_petkov2001@abv.bg>
2024-07-04 17:56:27 +0300

    Updates

--- data/data.js
+++ data/data.js
@@ -1,65 +1,82 @@
 export const ARTICLES = [
   {
     id: 1,
-    imgUrl: `/images/articles/article1.png`,
+    imgUrl: `images/articles/article1.png`,
     title: `USA, GA - Milton Eagles Football`,
     content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
       `,
   },
   {
     id: 2,
-    imgUrl: `/images/articles/article2.png`,
-    title: `USA, GA - Milton Eagles Football`,
-    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+    imgUrl: `images/articles/article2.png`,
+    title: `Milton, GA - Fury Performance Academy`,
+    content: `2021 Atlanta GA, Teremarkets and Fury Performance Academy are delighted to announce a new sponsorship and long-term partnership between the successful businesses. Teremarkets continues its support and collaboration with businesses, sport programs and communities all across the USA  
+ 
+    Fury Performance is focused on developing young athletes to their full potential, offering professional approach to both physical, and mental development“ Our vision is to set the standard of excellence for athletes in all ages and all type of sports by providing first class training programs, professional instruction, and life skills.” Commented JOE ELLIS JR. Fury Performance OWNER &amp; CEO
       `,
   },
   {
     id: 3,
-    imgUrl: `/images/articles/article3.png`,
-    title: `USA, GA - Milton Eagles Football`,
-    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+    imgUrl: `images/articles/article3.png`,
+    title: `US Team - Thank you for TEREMARKETS`,
+    content: `August 2022 - On behalf of all the USA Masters Team, we would like to extend a HUGE thank you to one of our major sponsors, Teremarkets! In this last year, these are just a few of the lives and places across the country and world that you have impacted through your generosity. This team could not fulfill its mission and vision without your support and WE THANK YOU!!!
       `,
   },
   {
     id: 4,
-    imgUrl: `/images/articles/article4.png`,
-    title: `USA, GA - Milton Eagles Football`,
-    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+    imgUrl: `images/articles/article4.png`,
+    title: `CORPORATE MEMBERSHIP - PALADIN ATHLETIC`,
+    content: `August 2022 – Teremarkets officially joined the Paladin Athletic Booster Club as a corporate sponsor!
+
+    “Thank you for joining the Paladin Athletic Booster Club for the 2022-2023 year as a Corporate Sponsor! We sincerely appreciate your support.
+    Together, Teremarkets and the Paladin Athletic Booster Club (PABC) aim to support and promote the advancement of Fellowship Christian School athletics.
       `,
   },
   {
     id: 5,
-    imgUrl: `/images/articles/article5.png`,
-    title: `USA, GA - Milton Eagles Football`,
-    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+    imgUrl: `images/articles/article5.png`,
+    title: `WA, USA Major Football Sponsorship`,
+    content: `ICE Sports Academy (Whatcom Wild) and S.C. Tigers (Lynwood) announces a major 3 years partnership deal with Teremarkets
+
+    Washington, USA, May 2020 - Whatcom Wild (Bellingham) and S.C. Tigers (Lynwood) announced  a multiyear agreement with Teremarkets for it to become the club official major sponsor.<br>
+    The three-year agreement, ranks among the biggest sponsorship deals in Washington youth football, will support the football associations to grow the youth football, offering a competitive environment for more young athletes to join the club while promoting healthier lifestyle.
       `,
   },
   {
     id: 6,
-    imgUrl: `/images/articles/article6.png`,
-    title: `USA, GA - Milton Eagles Football`,
-    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+    imgUrl: `images/articles/article6.png`,
+    title: `US Team - Masters Program`,
+    content: `April 2020 - USA Masters Team would like to extend out extreme gratitude towards Teremarkets in aiding our corporation to not only make a mark online, but a substantial statement of presence and digital awareness of our mission. 
+        	
+    “With Teremarkets help we have been able to cross borders and reach an audience of supporters that we could not have begun to fathom” Commented Trinity Osborn President of USMT "Our mission to support mature elite athletes, re-ignite a competitive fire within them, and aide in promoting an active, healthy lifestyle through fitness is being achieved, because of the partnership and efforts of Teremarkets."
       `,
   },
   {
     id: 7,
-    imgUrl: `/images/articles/article7.png`,
-    title: `USA, GA - Milton Eagles Football`,
-    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+    imgUrl: `images/articles/article7.png`,
+    title: `The Lions  - RBL U12 Basketball Team`,
+    content: `Vancouver, BC – The youth team 2019 RBL league champions announced that they have signed a major sponsorship deal with Teremarkets. Among many features of the deal, Teremarket’s new sponsorship will feature a new set of uniform, elite coaching sessions and more. 
+
+    “We are delighted and grateful for the opportunity Teremarkets give us allowing us to focus on developing the team and our competitive skills to try and run for a back to back championship also this season” Said Eddie K. the Lions head coach.
       `,
   },
   {
     id: 8,
-    imgUrl: `/images/articles/article8.png`,
-    title: `USA, GA - Milton Eagles Football`,
-    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+    imgUrl: `images/articles/article8.png`,
+    title: `USA National Quarterback Competition`,
+    content: `June 2019 – Teremarkets, a leading global digital marketing provider and the National Football Academy (NFA) today announced Teremarkets as the official sponsor and “marketing Technology Partner” for the 2019 NFA Duel National Quarterback Competition.
+    The partnership includes Teremarkets providing a range of digital and marketing services as well as extensive global exposer providing networking, marketing and infrastructure solutions.
+    
+    Ethan Halevi, Teremarkets Co-Founder, said, “It is an honor to work with the amazing team of the NFA, one of the most prestigious football development programs in the US.”
       `,
   },
   {
     id: 9,
-    imgUrl: `/images/articles/article9.png`,
-    title: `USA, GA - Milton Eagles Football`,
-    content: `USA, GA - May 2024 - We greatly appreciate and Thank TEREMARKETS for their support of our team, program, players and community! Without you we could not create such an amazing environment and program for these Athletes. GO EAGLES!
+    imgUrl: `images/articles/article9.png`,
+    title: `Monroe, WA Youth Football Program`,
+    content: `August 2019 - Monroe Football Program announced this week it has reached a multiyear agreement with Teremarkets for it to become one of the club’s official major sponsor. The agreement, will support the program to grow the program and its unique approach to competitive football in youth level. 
+
+    “We are thrilled to have been given the opportunity to strengthen our commitment to sport and community football in Washington state and the USA; We very much look forward to following the progress of all participating teams when the new season gets underway.” Said Ethan Halevi, Teremarkets Co-Founder and MD.
       `,
   },
 ];
